0aaf15aa2c385f69be6afed007464aa1f3942691
Chatano <caepradiee@gmail.com>
2025-01-10 01:28:02 -0300

    feat: craete styles folder and move globals.css to it

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next'
-import './globals.css'
+import '@/styles/globals.css'
 import { PropsWithChildren } from 'react'
 import { Fredoka, Changa } from 'next/font/google'
 
