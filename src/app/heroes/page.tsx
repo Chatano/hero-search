2e3afa4ccc6200da174cb46dd7d67a627d769c79
Chatano <caepradiee@gmail.com>
2025-01-10 19:28:09 -0300

    feat: create heroes page and filter bar

---
+++ src/app/heroes/page.tsx
@@ -0,0 +1,52 @@
+import { Input } from '@/components/input'
+import './styles.css'
+import { SearchIcon } from '@/assets/icons'
+import { Button } from '@/components/button'
+import { Dropdown } from '@/components/dropdown'
+
+type Filters = {
+  search?: string
+}
+
+interface Props {
+  searchParams?: Promise<Filters>
+}
+
+export default async function HeroesPage({ searchParams }: Props) {
+  const filters = await searchParams
+
+  return (
+    <main className="heroes__wrapper">
+      <div className="heroes__filters">
+        <Input
+          id="search-text"
+          wrapperClassName="flex-1"
+          leftContent={<SearchIcon size={16} />}
+          placeholder="Pesquise o nome aqui"
+          defaultValue={filters?.search}
+        />
+
+        <div className="heroes__filters__dropdowns">
+          <Dropdown
+            category="comics"
+            options={[
+              { id: 'A', label: 'A' },
+              { id: 'B', label: 'B' },
+              { id: 'C', label: 'C' },
+            ]}
+          />
+
+          <Dropdown
+            category="series"
+            options={[
+              { id: 'D', label: 'D' },
+              { id: 'E', label: 'E' },
+              { id: 'F', label: 'F' },
+            ]}
+          />
+        </div>
+        <Button>Buscar</Button>
+      </div>
+    </main>
+  )
+}
