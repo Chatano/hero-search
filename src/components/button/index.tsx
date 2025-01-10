19cbc61a25db4e1248aa1986b14fd2afec7a7ee3
Chatano <caepradiee@gmail.com>
2025-01-10 15:19:06 -0300

    feat: create button component

---
+++ src/components/button/index.tsx
@@ -0,0 +1,11 @@
+import clsx from "clsx"
+import { ComponentProps, FC } from "react"
+import './styles.css'
+
+export const Button: FC<ComponentProps<'button'>> = ({ className, children, ...props }) => {
+  return (
+    <button className={clsx("button__wrapper", className)} {...props}>
+      {children}
+    </button>
+  )
+}
