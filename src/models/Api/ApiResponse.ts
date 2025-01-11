160bbdac381141b1d30db64f825e0b9fbdb3325b
Chatano <caepradiee@gmail.com>
2025-01-10 21:23:43 -0300

    feat: create ApiResponse and Hero models

---
+++ src/models/Api/ApiResponse.ts
@@ -0,0 +1,16 @@
+export interface ApiResponse<T> {
+  code: number,
+	status: string,
+	copyright: string,
+	attributionText: string,
+	attributionHTML: string,
+	etag: string,
+  
+	data: {
+		offset: number,
+		limit: number,
+		total: number,
+		count: number,
+		results: T
+	}
+}
---
+++ src/models/Hero.ts
@@ -0,0 +1,11 @@
+export interface Hero {
+  id: number,
+  name: string,
+  description: string,
+  modified: string,
+  thumbnail: {
+    path: string,
+    extension: string,
+  },
+  resourceURI: string,
+}
