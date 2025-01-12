ea67a050577911178e5b8ca1e92f4fbf0ed390e6
Chatano <caepradiee@gmail.com>
2025-01-11 22:37:03 -0300

    chore: add marvel images hostname to next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'i.annihil.us',
+      },
+    ],
+  },
 }
 
 export default nextConfig
