801fe00c8884d998ce840b208a5b53bd80c43911
Chatano <caepradiee@gmail.com>
2025-01-12 01:23:45 -0300

    feat: create updateUrlParams util function

---
+++ src/utils/update-url-params.ts
@@ -0,0 +1,18 @@
+import { Filters } from "@/models/Filters";
+
+export const updateUrlParams = (searchParams: URLSearchParams, filters: Filters) => {
+  const updatedSearchParams = new URLSearchParams(searchParams);
+
+  const filterEntries = Object.entries(filters) // transform filters from obj in an array
+
+  filterEntries.forEach(([key, value]) => {
+    if (!value || value?.trim()?.length < 1) { // if null or be empty string
+      updatedSearchParams.delete(key)
+      return;
+    }
+
+    updatedSearchParams.set(key, value.toString()) // if exists, add to url
+  })
+
+  return updatedSearchParams.toString();
+}
