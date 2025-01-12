1e52f8000038faf201b9a961c19c8794c1362d35
Chatano <caepradiee@gmail.com>
2025-01-12 03:32:13 -0300

    feat: create pagination model and map from response method

---
+++ src/models/Pagination.ts
@@ -0,0 +1,33 @@
+import { ApiResponse } from "./Api/ApiResponse"
+
+export class Pagination {
+  public readonly hasNextPage: boolean = false
+  public readonly hasPrevPage: boolean = false
+  public readonly totalPages: number = 0
+  public readonly page: number = 1
+  public readonly totalItems: number = 0
+  public readonly pageItems: number = 0
+  public readonly limitPerPage: number = 20
+
+  static mapFromApiResponse(data: ApiResponse<unknown>['data']) {
+    const { limit, offset, total } = data
+
+    const hasNextPage = (offset + limit) < total
+    const hasPrevPage = offset > 0 
+    const totalPages = Math.ceil(total / limit)
+    const totalItems = data.total
+    const page = Math.floor(offset / limit) + 1
+    const pageItems = data.count
+    const limitPerPage = limit
+
+    return {
+      hasNextPage,
+      hasPrevPage,
+      totalPages, 
+      totalItems,
+      page,
+      pageItems,
+      limitPerPage,
+    }
+  }
+}
