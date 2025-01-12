1e1cb1c07bfb4f27f784438408524189cb85ee23
Chatano <caepradiee@gmail.com>
2025-01-12 12:54:56 -0300

    feat: create parse image url util

---
+++ src/utils/api/parse-image-url.ts
@@ -0,0 +1,8 @@
+import { Hero } from "@/models/Hero";
+
+export const parseImageUrl = (thumbnail?: Hero['thumbnail']) => {
+  const path = thumbnail?.path
+  const extension = thumbnail?.extension
+
+  return path && extension ? `${path}.${extension}` : null
+}
