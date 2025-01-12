641ebfecefdedfa5d7dcabdf86498cb2fe675172
Chatano <caepradiee@gmail.com>
2025-01-11 22:36:32 -0300

    feat: create get api url function

---
+++ src/utils/get-api-url.ts
@@ -0,0 +1,19 @@
+import { getApiHash } from './get-api-hash'
+
+const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL
+
+export const getApiURL = (endpoint: string, filters?: Array<[string, string]>) => {
+  const { hash, publicApiKey, timeStamp } = getApiHash()
+
+  const resolvedEndpoint = endpoint.slice(endpoint.startsWith('/') ? 1 : 0)
+
+  const url = new URL(resolvedEndpoint, baseURL)
+
+  url.searchParams.append('ts', timeStamp.toString())
+  url.searchParams.append('apikey', publicApiKey)
+  url.searchParams.append('hash', hash)
+  
+  filters?.forEach(([key, value]) => url.searchParams.append(key, value))
+
+  return url
+}
