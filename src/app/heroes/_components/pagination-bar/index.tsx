b1ec71cc4de6de924397f7329b68a2644b2085d8
Chatano <caepradiee@gmail.com>
2025-01-12 03:32:59 -0300

    feat: create pagination-bar component

---
+++ src/app/heroes/_components/pagination-bar/index.tsx
@@ -0,0 +1,42 @@
+'use client'
+import './styles.css'
+import { Pagination } from "@/models/Pagination"
+import { FC } from "react"
+import { ChevronLeft, ChevronRight } from 'lucide-react'
+import { useRouter, useSearchParams } from 'next/navigation'
+import { updateUrlParams } from '@/utils/filters/update-url-params'
+
+interface Props {
+  pagination: Pagination
+}
+
+export const HeroesPaginationBar: FC<Props> = ({ pagination }) => {
+  const router = useRouter()
+  const searchParams = useSearchParams()
+
+  const handleChangePage = (value: number) => {
+    if (value < 1) return;
+    
+    const updatedPage = Math.min(value, pagination.totalPages)
+
+    const updatedSearchParams = updateUrlParams(searchParams, { page: updatedPage })
+    router.push(`/heroes?${updatedSearchParams.toString()}`)
+  }
+
+  const handleGoBack = () => handleChangePage(pagination.page - 1)
+  const handleGoNext = () => handleChangePage(pagination.page + 1)
+
+  return (
+    <div className="heroes__pagination__wrapper">
+      <button onClick={handleGoBack} disabled={!pagination.hasPrevPage} className='heroes__pagination__arrow'>
+        <ChevronLeft size={20} />
+      </button>
+
+      <span className='heroes__pagination__current-page'>{pagination.page} of {pagination.totalPages}</span>
+
+      <button onClick={handleGoNext} disabled={!pagination.hasNextPage} className='heroes__pagination__arrow'>
+        <ChevronRight size={20} />
+      </button>
+    </div>
+  )
+}
