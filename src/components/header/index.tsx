fdcfc59c8e3435558a4e69a71d93774da0e0f560
Chatano <caepradiee@gmail.com>
2025-01-13 02:08:32 -0300

    feat: add style to current path at header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,6 +1,9 @@
+'use client'
 import Image from 'next/image'
 import Link from 'next/link'
 import './styles.css'
+import { usePathname } from 'next/navigation'
+import clsx from 'clsx'
 
 const tabs = [
   {
@@ -14,6 +17,8 @@ const tabs = [
 ]
 
 export const Header = () => {
+  const pathName = usePathname()
+
   return (
     <header className="header__wrapper">
       <Link href="/">
@@ -29,7 +34,7 @@ export const Header = () => {
       <nav>
         <ul className="header__tabs">
           {tabs.map((tab) => (
-            <li key={tab.path} className="header__tabs__item">
+            <li key={tab.path} className={clsx("header__tabs__item", pathName === tab.path && 'active')}>
               <Link href={tab.path}>{tab.label}</Link>
             </li>
           ))}
