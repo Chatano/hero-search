d590f2534cea7556ab1c4ced1c7bc3d3ac678c40
Chatano <caepradiee@gmail.com>
2025-01-14 16:49:44 -0300

    test: create initial values tests at heroes filters

--- cypress/e2e/heroes-page.cy.ts
+++ cypress/e2e/heroes-page.cy.ts
@@ -4,11 +4,30 @@ describe('[E2E] Heroes Page', () => {
     cy.viewport(1920, 1080)
   })
 
-  it('should render initially 10 heroes', () => {
-    cy.get('.hero-card').should('have.length', 10)
+  describe('Render & UI', () => {
+    it('should render header and highlight heroes link only', () => {
+      cy.validatePageHeader('heroes')
+    })
   })
 
-  it('should render header and highlight heroes link only', () => {
-    cy.validatePageHeader('heroes')
+  describe('Filters & Pagination', () => {    
+    it('should have sort dropdown with A-Z by default', () => {
+      cy.get('#order-dropdown').contains('A-Z')
+    })
+    it('should have pageSize dropdown containing 10 by default', () => {
+      cy.get('#page-size-dropdown').contains('10')
+    })
+    it('should have empty search input by default', () => {
+      cy.get('#search-text').should('have.value', '')
+    })
+    it('should be at page 1 by default', () => {
+      cy.get('#current-page').contains(/^1 of(.*)$/)
+    })
+  })
+
+  describe('Fetch & query API data', () => {
+    it('should render initially 10 heroes', () => {
+      cy.get('.hero-card').should('have.length', 10)
+    })
   })
 })
